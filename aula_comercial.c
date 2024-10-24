cd741a06572d6073fc98b5f62b8e98b236486ae4
felipe251957 <35401921+felipe251957@users.noreply.github.com>
2024-10-24 21:58:48 +0000

    Aula inicial

---
+++ aula_comercial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+    //sintaze scanf
+    //scanf("formato1" "formato2", &variavel1, &variavel2,......);
+    printf ("Entre com sua idade e altura\n");
+    scanf("%d", &idade);
+    printf("Idade: %d\n", idade);
+    printf("Entre com a altura\n");
+    scanf("%f", &altura);
+    printf("Entre com a opcao\n");
+    scanf(" %c", &opcao);
+    printf("opção é:%c\n", opcao); 
+    
+
+}
---
+++ aula_printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+int idade = 25;
+float altura = 1.75;
+char opção = 's';
+char nome[20] = "felipe";
+
+printf ("%d", idade);
+printf ("%f", altura);
+printf ("%c", opção);
+printf ("%s", nome);
+/*
+printf("%formato1 %formato2 %formato3", variavel1, variavel2, variavel3)
+
+%d: Imprime u inteiro no formato decimal
+%i: Equivalente a %d.
+%f: Imprime um número de ponto flutuante no formato padrão.
+%e: Imprime um número de ponto flutuante na notação cientifica.
+%c: Imprime um único caractere
+%s: Imprime uma cadeira (string) de caracteres.
+*/
+
+}
